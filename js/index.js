99a25ccd5fc45bfaf096a203df5611f519e8c9b9
Aliaksei Karzhou <alexius90e@gmail.com>
2025-12-28 22:56:27 +0300

    feat: added reviews

--- js/index.js
+++ js/index.js
@@ -14,12 +14,50 @@ if (trainingSystemSwiper && trainingSystemPagination) {
     loop: true,
     slidesPerView: 'auto',
     spaceBetween: 20,
+    allowTouchMove: false,
+    breakpoints: {
+      320: {
+        allowTouchMove: false,
+      },
+      769: {
+        allowTouchMove: true,
+      },
+    },
     pagination: {
       el: trainingSystemPagination,
       clickable: true,
       renderBullet: function (index, className) {
         return '<span class="' + className + '">' + (index + 1) + '</span>';
       },
+    },
+  });
+}
+
+const reviewsSwiper = document.querySelector('.reviews .swiper');
+const reviewsPagination = document.querySelector('.reviews__cards-pagination');
+const reviewsPrev = document.querySelector('.reviews__cards-prev');
+const reviewsNext = document.querySelector('.reviews__cards-next');
+
+if (reviewsSwiper) {
+  const swiper = new Swiper(reviewsSwiper, {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+
+      769: {
+        slidesPerView: 2,
+      },
+
+      1025: {
+        slidesPerView: 3,
+      },
+    },
+    pagination: {
+      el: trainingSystemPagination,
     },
   });
 }
